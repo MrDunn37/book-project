42ea3aeb5b7c8f1d43501030e70b217c5d3aeec1
Preston Dunn <prestondunn@Prestons-MacBook-Pro.local>
2022-07-03 22:11:01 -0400

    Inital Commit

---
+++ app.py
@@ -0,0 +1,8 @@
+# import modles
+# main menu - add, search, analysis, exit, view
+# add books to the DB function
+# edit books function
+# delete books function
+# search function
+# data cleaning function
+# loop runs program
---
+++ models.py
@@ -0,0 +1,4 @@
+#create a Database
+#books.db
+#create model
+#title, author, date published, price
