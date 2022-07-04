872bf61655ea3d33ffbc8980f00c7a862a44b9b9
Preston Dunn <prestondunn@Prestons-MacBook-Pro.local>
2022-07-03 22:25:37 -0400

    added our DB modle

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # import modles
+from models import (Base, session, Book, engine)
 # main menu - add, search, analysis, exit, view
 # add books to the DB function
 # edit books function
@@ -6,3 +7,7 @@
 # search function
 # data cleaning function
 # loop runs program
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
